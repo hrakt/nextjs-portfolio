6343e43fc84df24660010c6accfcdee139add650
Steve Scavo <stevescavo@gmail.com>
2019-10-10 12:43:57 -0700

    Add meta tags

--- layouts/main.js
+++ layouts/main.js
@@ -7,6 +7,13 @@ import Footer from '../components/Footer/Footer';
 export default ({ children }) => (
     <div>
         <Head>
+            <meta charSet="utf-8" />
+            <meta http-equiv="x-ua-compatible" content="ie=edge" />
+            <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1"
+            />
+            <title>Hrak Torousan</title>
             <link
                 href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900&display=swap"
                 rel="stylesheet"
