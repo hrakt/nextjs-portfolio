24ab6f1823beddfbfbfd4466e6ee676266e6ab9c
Hrak Torousian <hraktorres@gmail.com>
2019-10-16 17:43:48 -0700

    switch to link as method for prettier url (#3)

--- pages/projects/[id].js
+++ pages/projects/[id].js
@@ -1,12 +1,12 @@
 import { useRouter } from 'next/router';
-import Layout from '../layouts/main';
-import ProjectPage from '../components/Portfolio/ProjectPage';
-import projects from '../utils/projects';
+import Layout from '../../layouts/main';
+import ProjectPage from '../../components/Portfolio/ProjectPage';
+import projects from '../../utils/projects';
 
 const Project = () => {
     const router = useRouter();
     const project = projects.find(element => {
-        return element.title == router.query.title;
+        return element.id == router.query.id;
     });
 
     return <Layout>{project && <ProjectPage project={project} />}</Layout>;
