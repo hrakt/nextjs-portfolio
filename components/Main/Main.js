72c606e2d7119518b7aadb3b63218dd469fd483c
hrakt <hraktorres@gmail.com>
2019-10-21 14:41:16 -0700

    add animation for emoji wave

--- components/Main/Main.js
+++ components/Main/Main.js
@@ -1,13 +1,20 @@
 import main from '../Main/Main.scss';
 import Button from '../common/Button';
-import Link from 'next/link';
+import { motion } from 'framer-motion';
 
 const Main = () => {
     return (
         <div className={main['container']}>
             <div className={main['main-container']}>
                 <h1 className={main['main-container__heading1']}>
-                    Hi, I'm Hrak 👋
+                    Hi, I'm Hrak{' '}
+                    <motion.div
+                        className={main['main-container__emoji']}
+                        animate={{ rotate: [0, 0, 90, 90, 0] }}
+                        transition={{ duration: 2 }}
+                    >
+                        👋
+                    </motion.div>
                 </h1>
                 <h2 className={main['main-container__heading2']}>
                     I'm a
