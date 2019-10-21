9ab39b976ea4a7fdda99916172a7066ce2696d94
Hrak Torousian <hraktorres@gmail.com>
2019-10-21 10:56:51 -0700

    Revert "Feature/visual changes (#5)"
    This reverts commit 66736427f0d2303b02011215af59c1dee812b8fd.

--- components/Portfolio/Portfolio.js
+++ components/Portfolio/Portfolio.js
@@ -4,29 +4,13 @@ import projects from '../../utils/projects';
 import Link from 'next/link';
 import Filter from '../Portfolio/Filter';
 
-import { useState } from 'react';
-
-
-
-const getProjects = filters => {
-    if (filters.length > 0) {
-        return projects.filter(item =>
-            item.stack.some(r => filters.includes(r))
-        );
-    }
-    return projects;
-};
-
-const returnProjects = filteredProjects => {
-    return filteredProjects.map(project => {
+const getProjects = () => {
+    return projects.map(project => {
         return (
             <div
                 key={project.id}
                 className={portfolio['portfolio-container__content']}
             >
-
-
-
 
                 <div
                     className={portfolio['portfolio-container__box']}
@@ -36,23 +20,29 @@ const returnProjects = filteredProjects => {
                         <div>
                             <img
                                 src={project.picture}
-
-                                alt="Portfolio-1"
-                                className={
+                                alt="Portfolio-1" 
+ className={
                                     portfolio['portfolio-container__img']
                                 }
-                            />
+/>
 
-                            <div
-                                className={
-                                    portfolio['portfolio-container__box']
-                                }
-                            ></div>
-
-                                             </div>
+                <div className={portfolio['portfolio-container__box']}>
+                    <img
+                        src={project.picture}
+                        alt="Portfolio-1"
+                        className={portfolio['portfolio-container__img']}
+                    />
+                    <div className={portfolio['portfolio-container__buttons']}>
+                        <Button
+                            id={project.id}
+                            type="internal"
+                            title="Learn More"
+                            href={`/project?title=${project.title}`}
+                        />
+                    </div>
+                </div>
 
              
-
 
                             <div
                                 className={
@@ -74,10 +64,9 @@ const returnProjects = filteredProjects => {
 };
 
 const Portfolio = () => {
-    const [filters, setFilters] = useState([]);
     return (
         <React.Fragment>
-            <Filter selectedItems={filters} setItems={setFilters} />
+            <Filter />
             <div className={portfolio['container-3']} id="portfolio">
                 <div className={portfolio['portfolio-container']}>
                     <h4 className={portfolio['portfolio-container__heading']}>
@@ -87,9 +76,7 @@ const Portfolio = () => {
                         Check out my latest projects
                     </h5>
                     <div className={portfolio['portfolio-container__projects']}>
-
-                        {returnProjects(getProjects(filters))}
-
+                        {getProjects()}
                     </div>
                 </div>
             </div>
