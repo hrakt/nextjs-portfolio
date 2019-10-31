0f76e846d0b431f41d7bf7b9c4309292f203283c
hrakt <hraktorres@gmail.com>
2019-10-30 18:14:19 -0700

    Add error message for no results were found

--- components/Portfolio/Projects.js
+++ components/Portfolio/Projects.js
@@ -18,53 +18,67 @@ const getProjects = filters => {
 const Projects = ({ filters }) => {
     const filteredProjects = getProjects(filters);
     render++;
-    return filteredProjects.map(project => {
-        const tagString =
-            `#` + project.stack.toString().replace(new RegExp(',', 'g'), ' #');
 
-        return (
-            <motion.div
-                animate={{ opacity: [0, 1] }}
-                transition={{ duration: 1 }}
-                key={`${render}_${project.id}`}
-                className={styles['portfolio-container__content']}
-            >
-                <div
-                    className={styles['portfolio-container__box']}
-                    href={`/project?title=${project.title}`}
+    if (filteredProjects.length > 0) {
+        return filteredProjects.map(project => {
+            const tagString =
+                `#` +
+                project.stack.toString().replace(new RegExp(',', 'g'), ' #');
+
+            return (
+                <motion.div
+                    animate={{ opacity: [0, 1] }}
+                    transition={{ duration: 1 }}
+                    key={`${render}_${project.id}`}
+                    className={styles['portfolio-container__content']}
                 >
-                    <Link href={`/projects/${project.id}`}>
-                        <div>
-                            <img
-                                src={project.picture}
-                                alt="Portfolio-1"
-                                className={styles['portfolio-container__img']}
-                            />
+                    <div
+                        className={styles['portfolio-container__box']}
+                        href={`/project?title=${project.title}`}
+                    >
+                        <Link href={`/projects/${project.id}`}>
+                            <div>
+                                <img
+                                    src={project.picture}
+                                    alt="Portfolio-1"
+                                    className={
+                                        styles['portfolio-container__img']
+                                    }
+                                />
 
-                            <div
-                                className={
-                                    styles['portfolio-container__buttons']
-                                }
-                            >
                                 <div
                                     className={
-                                        styles['portfolio-container__tags']
+                                        styles['portfolio-container__buttons']
                                     }
                                 >
-                                    {tagString}
+                                    <div
+                                        className={
+                                            styles['portfolio-container__tags']
+                                        }
+                                    >
+                                        {tagString}
+                                    </div>
+                                    <Button
+                                        id={project.id}
+                                        type="internal"
+                                        title="Learn More"
+                                    />
                                 </div>
-                                <Button
-                                    id={project.id}
-                                    type="internal"
-                                    title="Learn More"
-                                />
                             </div>
-                        </div>
-                    </Link>
-                </div>
-            </motion.div>
+                        </Link>
+                    </div>
+                </motion.div>
+            );
+        });
+    } else {
+        return (
+            <React.Fragment>
+                <p className={styles['portfolio-container__error']}>
+                    No results were found
+                </p>
+            </React.Fragment>
         );
-    });
+    }
 };
 
 export default Projects;
