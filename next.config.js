1aab88fab80579ec700e1f99404d599f5b096c34
Hrak Torousian <hraktorres@gmail.com>
2020-12-19 22:23:32 -0800

    Change next.config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
-const withSass = require("@zeit/next-sass"); // Add this declaration
-const withImages = require("next-images");
-module.exports = withImages(
-    withSass({
-        cssModules: true,
-        cssLoaderOptions: {
-            importLoaders: 1,
-            localIdentName: "[local]___[hash:base64:5]"
-        }
-    })
-);
+module.exports = {
+	experimental: { css: true },
+}
